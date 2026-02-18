c9233663ae0d2cc48649a2e94afb225e04c099e6
leanclientportal <leanclientportal@gmail.com>
2026-02-18 18:09:49 +0530

    Add files via upload

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,1 +1,31 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import ProblemSection from "@/components/ProblemSection";
+import SolutionSection from "@/components/SolutionSection";
+import HowItWorks from "@/components/HowItWorks";
+import UseCases from "@/components/UseCases";
+import Features from "@/components/Features";
+import TrustSection from "@/components/TrustSection";
+import Pricing from "@/components/Pricing";
+import FinalCTA from "@/components/FinalCTA";
+import Footer from "@/components/Footer";
 
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <Hero />
+      <ProblemSection />
+      <SolutionSection />
+      <HowItWorks />
+      <UseCases />
+      <Features />
+      <TrustSection />
+      <Pricing />
+      <FinalCTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
